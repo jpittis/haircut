e5fc1ed4e50062e29e4f66974c60989581b16cbd
Jake Pittis <jakepittis@gmail.com>
2018-07-05 20:07:59 -0400

    add super sketchy benchmark

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,8 +4,10 @@ import qualified TVar
 import qualified MVar
 import Generic
 
-import Control.Concurrent (threadDelay)
-import Control.Monad      (forM_)
+import Control.Concurrent       (threadDelay)
+import Control.Monad            (forM_)
+import Control.Concurrent.Async (replicateConcurrently_)
+import Data.Time.Clock.POSIX    (getPOSIXTime)
 
 main :: IO ()
 main = hspec $ do
@@ -26,6 +28,24 @@ main = hspec $ do
     it "resets after errorTimeout without tripping" $ do
       mvar <- MVar.new Config { errorThreshold = 3, errorTimeout = 2 }
       runTest2 mvar
+
+  describe "Benchmark" $ do
+    it "runs a bunch of successes" $ do
+      forM_ [1..10] $ \_ -> do
+        print "---"
+        mvar <- MVar.new Config { errorThreshold = 3, errorTimeout = 2 }
+        (time $ benchmark 10000 mvar) >>= print
+        tvar <- TVar.new Config { errorThreshold = 3, errorTimeout = 2 }
+        (time $ benchmark 10000 tvar) >>= print
+
+time f = do
+  before <- currentTime
+  f
+  after <- currentTime
+  return (after - before)
+  where
+    currentTime :: IO Int
+    currentTime = round . (* 1000) <$> getPOSIXTime
 
 runTest1 circuit =
   forM_ [1..3] $ \_ -> do
@@ -48,6 +68,12 @@ runTest2 circuit =
     withCircuitBreaker circuit failure `shouldThrow` anyException
     withCircuitBreaker circuit success `shouldReturn` Just ()
     sleep 2
+
+benchmark n circuit =
+  replicateConcurrently_ n (makeNRequests n)
+  where
+    makeNRequests n = forM_ [1..n] $ \_ ->
+      withCircuitBreaker circuit success
 
 success :: IO ()
 success = return ()
